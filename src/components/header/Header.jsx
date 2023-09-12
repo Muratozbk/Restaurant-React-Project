f2f8991407362fa7c02abad28003d47be33e88b0
muratozbk <slipstream11@yandex.com>
2023-09-12 12:10:57 +0300

    navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Navigation from './Navigation'
 
 export default function Header() {
     return (
-        <div>Header</div>
+        <div>
+            <Navigation />
+        </div>
     )
 }
+
+// 5.1.3
