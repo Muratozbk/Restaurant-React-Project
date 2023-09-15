53e0c3f669808b5b704ec48e4804fa38d8792db0
muratozbk <slipstream11@yandex.com>
2023-09-15 18:22:27 +0300

    footer component

--- src/components/body/About.jsx
+++ src/components/body/About.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { Container } from 'react-bootstrap'
 
 export default function About() {
     return (
-        <div>About</div>
+        <div>
+            <Fragment>
+                <Container>
+                    <h1>About Page</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quasi fuga sed?
+                        <br />
+                        voluptatum. Adipisci, atque. Laboriosam dolore pariatur vel sequi enim, nostrum eaque neque, quisquam accusantium, consequatur explicabo distinctio qui</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem repudiandae dignissimos, nemo soluta officia, accusamus quibusdam cum nisi numquam recusandae temporibus, sequi repellat molestiae provident ut odio cumque ex soluta vitae nostrum, sunt non obcaecati doloribus nam, consectetur libero quaerat reiciendis facilis rerum ullam voluptate, voluptas iure impedit. Libero quis, veniam velit incidunt maiores ducimus repudiandae, qui porro, deleniti sunt quidem reprehenderit in molestiae!</p>
+                    <p> voluptatum. Adipisci, atque. Laboriosam dolore pariatur vel sequi enim, nostrum eaque neque, quisquam accusantium, consequatur explicabo distinctio qui</p>
+                </Container>
+            </Fragment>
+        </div >
     )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import '../../assets/css/Main.css'
 
 export default function Footer() {
     return (
-        <div>Footer</div>
+        <div className='footer'>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-auto">
+                        <p>Easy Restaurant @ Copyright 2023</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
