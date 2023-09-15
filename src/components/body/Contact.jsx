9f79d5693f5c4ac791142130fb11b2d45dbfa1d8
muratozbk <slipstream11@yandex.com>
2023-09-15 12:25:48 +0300

    contact form

--- src/components/body/Contact.jsx
+++ src/components/body/Contact.jsx
@@ -1,9 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { Container, Col, Form, Row, Button, FloatingLabel } from 'react-bootstrap'
 
 export class Contact extends Component {
     render() {
         return (
-            <div>Contact</div>
+            <div>
+                <Fragment>
+                    <Container>
+                        <h1>Send us your Feedback</h1>
+                        <Form>
+                            <Row className="mb-3">
+                                <Form.Group as={Col} >
+                                    <Form.Label>First Name</Form.Label>
+                                    <Form.Control type="text" placeholder="First Name" />
+                                </Form.Group>
+
+                                <Form.Group as={Col} >
+                                    <Form.Label>Last Name</Form.Label>
+                                    <Form.Control type="text" placeholder="Last Name" />
+                                </Form.Group>
+                            </Row>
+
+                            <Row className="mb-3">
+                                <Form.Group as={Col} >
+                                    <Form.Label>Mobile</Form.Label>
+                                    <Form.Control type="number" placeholder="Mobile" />
+                                </Form.Group>
+
+                                <Form.Group as={Col} >
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control type="email" placeholder="Enter Your Email" />
+                                </Form.Group>
+                            </Row>
+
+                            <FloatingLabel controlId="floatingTextarea" label="Message" className='mb-3'>
+                                <Form.Control as='textarea' placeholder="Leave a Message Here" />
+                            </FloatingLabel>
+
+                            <Form.Label>Message</Form.Label>
+                            <Form.Group className="mb-3" id="formGridCheckbox">
+                                <Form.Check type="checkbox" label="May we contact you?" />
+                            </Form.Group>
+
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+                    </Container>
+                </Fragment>
+            </div >
         )
     }
 }
